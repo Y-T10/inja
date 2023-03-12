f5f380f31b52ab2a5a23211bb9069c8483d13531
Y-T10 <tayuji111@gmail.com>
2023-03-12 16:38:26 +0900

    single_includeを生成するようにした

--- scripts/update_single_include.sh
+++ scripts/update_single_include.sh
@@ -3,4 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $(dirname "${DIR}")
 
+mkdir -p "single_include/inja"
+touch "single_include/inja/inja.hpp"
 python3 third_party/amalgamate/amalgamate.py -c scripts/amalgamate_config.json -s include -v yes
